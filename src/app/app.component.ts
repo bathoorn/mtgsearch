ad0599d246cd91fee4df04045895f6cd85bd6ed1
ronnie <r.bathoorn@crystalloids.com>
2017-10-26 15:39:37 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,34 +29,4 @@ export class AppComponent {
           }
         );
     }
-  
-    onAddTodo(todo) {
-      this.todoDataService
-        .addTodo(todo)
-        .subscribe(
-          (newTodo) => {
-            this.todos = this.todos.concat(newTodo);
-          }
-        );
-    }
-  
-    onToggleTodoComplete(todo) {
-      this.todoDataService
-        .toggleTodoComplete(todo)
-        .subscribe(
-          (updatedTodo) => {
-            todo = updatedTodo;
-          }
-        );
-    }
-  
-    onRemoveTodo(todo) {
-      this.todoDataService
-        .deleteTodoById(todo.id)
-        .subscribe(
-          (_) => {
-            this.todos = this.todos.filter((t) => t.id !== todo.id);
-          }
-        );
-    }
 }
