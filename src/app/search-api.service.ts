e979d7173990e3a67224a351e897ee434a091b56
ronnie <r.bathoorn@crystalloids.com>
2017-10-26 15:58:58 +0200

    callback

--- src/app/search-api.service.ts
+++ src/app/search-api.service.ts
@@ -15,7 +15,7 @@ export class SearchApiService {
     return this.http.request(API_URL,{
       method: 'JSONP',
       params: {
-          'json.wrf': 'JSON_CALLBACK',
+          'json.wrf': 'callback',
           'q': 'goblins', 
           'wt': 'json',
           'rows': 25,
