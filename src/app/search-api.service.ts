0a2b646268f175ba96a5e08ed3009eb9114ccc4d
ronnie <r.bathoorn@crystalloids.com>
2017-10-26 15:11:26 +0200

    add results

--- src/app/search-api.service.ts
+++ src/app/search-api.service.ts
@@ -6,9 +6,24 @@ const API_URL = environment.searchUrl;
 
 @Injectable()
 export class SearchApiService {
+  results = undefined;
 
-  constructor(
-    private http: Http
-  ) {
+  constructor(private http: Http) {
+  }
+  
+  searchSolr(): JSON {
+    this.http.request(API_URL,{
+      method: 'JSONP',
+      params: {
+          'json.wrf': 'JSON_CALLBACK',
+          'q': 'goblins', 
+          'wt': 'json',
+          'rows': 25,
+          'indent':true,
+          'facet':'true',
+          'facet.field' : 'colors'
+      }}
+    ).subscribe(res => this.results = res.json());
+    return this.results;
   }
 }
